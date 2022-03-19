4208d9800a4afde8859cf4f3f89fbd60e82b07e6
Maximilian Heim <MaximilianHeim@protonmail.com>
2022-03-19 18:52:22 +0100

    added functionality to chess board

--- src/fce.c
+++ src/fce.c
@@ -1,11 +1,65 @@
 #include "fce.h"
-#include <memory.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 
 int main(int argc, char *argv[]) {
+    printf("Initializing board...\n");
+    FILE *standard_setup_file = fopen("../assets/standard_setup", "r");
+    char standard_setup[100];
     fce_board *board = make_fce_board();
+    init_fce_board(board);
+    printf("Printing board...\n");
+    print_fce_board(board);
 }
 
 fce_board *make_fce_board() {
-    fce_board *board = malloc(sizeof(fce_board));
+    fce_board *board = calloc(1, sizeof(fce_board));
     return board;
+}
+void init_fce_board(fce_board *board) {
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 8; j++) {
+            switch(j) {
+                case 1:
+                board->field[i][j].type = pawn;
+                board->field[i][j].color = white;
+                case 6:
+                board->field[i][j].type = pawn;
+                board->field[i][j].color = black;
+            }
+        }
+    }
+}
+
+void print_fce_board(fce_board *board) {
+    for(char i = 0; i < 8; i++) {
+        for(char j = 0; j < 8; j++) {
+            printf("%c", get_symbol(board->field[i][j].type));
+        }
+        printf("\n");
+    }
+}
+
+char get_symbol(piece_types type) {
+    switch (type) {
+        case pawn:
+            return 'p';
+        case knight:
+            return 'n';
+        case bishop:
+            return 'b';
+        case king:
+            return 'k';
+        case rook:
+            return 'r';
+        case queen:
+            return 'q';
+        case empty:
+            return ' ';
+    }
+}
+fce_board *get_fce_board(fen fen_string) {
+
 }
--- src/fce.h
+++ src/fce.h
@@ -5,11 +5,31 @@
 #define PAWN_WORTH = 1
 #define BISHOP_WORTH = 3
 #define KNIGHT_WORTH = 3
+typedef char *fen;
 
+typedef enum {
+    white,
+    black
+} colors;
+typedef enum {
+    empty = 0,
+    pawn,
+    knight,
+    bishop,
+    queen,
+    king,
+    rook,
+} piece_types;
+typedef int bool2;
 typedef struct {
-    char piece[8][8];
-
+    piece_types type;
+    bool2 color; // 0 if black 1 if white
+} fce_piece;
+typedef struct {
+    fce_piece field[8][8];
 } fce_board;
-
 fce_board *make_fce_board();
+char get_symbol(piece_types type);
+void print_fce_board(fce_board *board);
+void init_fce_board(fce_board *board);
 #endif
