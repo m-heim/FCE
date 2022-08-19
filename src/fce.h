df49601316018dbe46ad67a5843a7ec9711adf9f
m-heim <MaximilianHeim@protonmail.com>
2022-08-20 00:51:18 +0200

    more changes

--- src/fce.h
+++ src/fce.h
@@ -5,13 +5,13 @@
 #define PAWN_WORTH = 1
 #define BISHOP_WORTH = 3
 #define KNIGHT_WORTH = 3
-typedef char *fen;
 
-typedef enum {
+enum fce_color{
     white,
     black
-} colors;
-typedef enum {
+};
+
+enum piece_type{
     empty = 0,
     pawn,
     knight,
@@ -19,11 +19,11 @@ typedef enum {
     queen,
     king,
     rook,
-} piece_types;
-typedef int bool2;
+};
+
 typedef struct {
-    piece_types type;
-    bool2 color; // 0 if black 1 if white
+    enum piece_type type;
+    enum fce_color color;
 } fce_piece;
 typedef struct {
     fce_piece field[8][8];
