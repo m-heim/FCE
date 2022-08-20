3e2cf52db72c16cda2c529fa9acf93e5ff4dafd3
m-heim <MaximilianHeim@protonmail.com>
2022-08-20 18:55:07 +0200

    added changes

--- src/fce.h
+++ src/fce.h
@@ -1,35 +1,58 @@
 #ifndef FCE_H
 #define FCE_H
+#include "stdint.h"
+#include <cstdint>
+#include <stdint.h>
 #define QUEEN_WORTH = 9
 #define ROOK_WORTH = 5
 #define PAWN_WORTH = 1
 #define BISHOP_WORTH = 3
 #define KNIGHT_WORTH = 3
 
-enum fce_color{
-    white,
-    black
+const uint64_t a_file = 0x0101010101010101;
+const uint64_t b_file = a_file << 1;
+const uint64_t c_file = b_file << 1;
+const uint64_t d_file = c_file << 1;
+const uint64_t e_file = d_file << 1;
+const uint64_t f_file = e_file << 1;
+const uint64_t g_file = f_file << 1;
+const uint64_t h_file = g_file << 1;
+
+const uint64_t first_rank = 0xFF;
+const uint64_t second_rank = first_rank << (1 * 8);
+const uint64_t third_rank = second_rank << (1 * 8);
+const uint64_t fourth_rank = third_rank << (1 * 8);
+const uint64_t fifth_tank = fourth_rank << (1 * 8);
+const uint64_t sixth_rank = fifth_tank << (1 * 8);
+const uint64_t seventh_rank = sixth_rank << (1 * 8);
+const uint64_t eighth_rank = seventh_rank << (1 * 8);
+
+const uint64_t queenside = a_file | b_file | c_file | d_file;
+const uint64_t kingside = e_file | f_file | g_file | h_file;
+const uint64_t center = (d_file | e_file) & (fourth_rank | fifth_tank);
+
+enum color{
+    WHITE,
+    BLACK
 };
 
-enum piece_type{
-    empty = 0,
-    pawn,
-    knight,
-    bishop,
-    queen,
-    king,
-    rook,
+enum board_type{
+    PAWN_BB,
+    KNIGHT_BB,
+    BISHOP_BB,
+    ROOK_BB,
+    QUEEN_BB,
+    KING_BB,
+    WHITE_BOARD_BB,
+    BLACK_BB
 };
 
-typedef struct {
-    enum piece_type type;
-    enum fce_color color;
-} fce_piece;
-typedef struct {
-    fce_piece field[8][8];
-} fce_board;
-fce_board *make_fce_board();
-char get_symbol(piece_types type);
-void print_fce_board(fce_board *board);
-void init_fce_board(fce_board *board);
+struct bitboard {
+    uint64_t board[8];
+};
+
+struct bitboard *make_bitboard();
+void print_fce_board(struct bitboard);
+void init_fce_board(struct bitboard *board);
+struct bitboard *move_piece();
 #endif
