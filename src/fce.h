9a0f9bb976d1a64a2b344692c87d2fa2beaa4a93
m-heim <MaximilianHeim@protonmail.com>
2022-08-20 19:42:09 +0200

    update h

--- src/fce.h
+++ src/fce.h
@@ -22,14 +22,19 @@ const uint64_t first_rank = 0xFF;
 const uint64_t second_rank = first_rank << (1 * 8);
 const uint64_t third_rank = second_rank << (1 * 8);
 const uint64_t fourth_rank = third_rank << (1 * 8);
-const uint64_t fifth_tank = fourth_rank << (1 * 8);
-const uint64_t sixth_rank = fifth_tank << (1 * 8);
+const uint64_t fifth_rank = fourth_rank << (1 * 8);
+const uint64_t sixth_rank = fifth_rank << (1 * 8);
 const uint64_t seventh_rank = sixth_rank << (1 * 8);
 const uint64_t eighth_rank = seventh_rank << (1 * 8);
 
 const uint64_t queenside = a_file | b_file | c_file | d_file;
 const uint64_t kingside = e_file | f_file | g_file | h_file;
-const uint64_t center = (d_file | e_file) & (fourth_rank | fifth_tank);
+const uint64_t center = (d_file | e_file) & (fourth_rank | fifth_rank);
+const uint64_t extended_center = (c_file | d_file | e_file | f_file) & (third_rank | fourth_rank | fifth_rank | sixth_rank);
+
+const uint64_t white_side = first_rank | second_rank | third_rank | fourth_rank;
+
+const uint64_t black_side = fifth_rank | sixth_rank | seventh_rank | eighth_rank;
 
 enum color{
     WHITE,
@@ -43,13 +48,15 @@ enum board_type{
     ROOK_BB,
     QUEEN_BB,
     KING_BB,
-    WHITE_BOARD_BB,
+    WHITE_BB,
     BLACK_BB
 };
 
 struct bitboard {
     uint64_t board[8];
 };
+
+void fce_error(const char *message);
 
 struct bitboard *make_bitboard();
 void print_fce_board(struct bitboard);
