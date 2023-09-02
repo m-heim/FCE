46855914b4b3d441ea0bc3dd84914414abd8ebbf
m-heim <MaximilianHeim@protonmail.com>
2023-09-03 01:09:12 +0200

    Add transposition table

--- src/position/transposition.cpp
+++ src/position/transposition.cpp
@@ -1,3 +1,17 @@
 #include "transposition.hpp"
 
 std::array<PositionInfo, TRANSPOSITION_TABLE_N> transpositionTable;
+
+void initTranspositionTable() {
+    for (uint64_t index = 0; index < TRANSPOSITION_TABLE_N; index++) {
+        transpositionTable.at(index) = PositionInfo();
+    }
+}
+
+PositionInfo getPositionInfo(Bitboard hash) {
+    return transpositionTable[hash % TRANSPOSITION_TABLE_N];
+}
+
+void insertPositionInfo(PositionInfo position) {
+    transpositionTable[position.hash % TRANSPOSITION_TABLE_N] = position;
+}
