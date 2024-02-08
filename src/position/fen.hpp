d8dbf0046f92cbbd1e52fc85e759407c7a9b1973
m-heim <MaximilianHeim@protonmail.com>
2024-02-08 20:45:41 +0100

    Add descriptions

--- src/position/fen.hpp
+++ src/position/fen.hpp
@@ -4,6 +4,18 @@
 #include "magic.hpp"
 #include "position.hpp"
 
+/**
+ * @brief Parse a FEN string
+ * @exception May fail if FEN invalid!
+ * @param fen Reference to FEN string
+ * @return Position that equals input FEN string
+ */
 Position parseFen(const std::string &fen);
 
+/**
+ * @brief Read a FEN from a file
+ * 
+ * @param fenPath The path to the file containing FEN string
+ * @return std::string the FEN string read from the file
+ */
 std::string readFen(const std::string &fenPath);
--- src/position/util.hpp
+++ src/position/util.hpp
@@ -2,4 +2,10 @@
 
 #include <string>
 
+/**
+ * @brief Raise a critical error and exit
+ * 
+ * @param message The message to display to stdout
+ * @param exit_code Exit code, usually 1 or 2
+ */
 void fce_error(const std::string &message, int exit_code);
