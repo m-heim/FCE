6d206633eeef631c6775829d3c63102fcf567d90
m-heim <MaximilianHeim@protonmail.com>
2024-03-24 16:17:20 +0100

    Use logger for fce_error

--- src/position/util.cpp
+++ src/position/util.cpp
@@ -1,8 +1,9 @@
 #include "util.hpp"
+#include "logger.hpp"
 #include <iostream>
 #include <string>
 
 void fce_error(const std::string &message, int exit_code) {
-    std::cerr << message << std::endl;
+    fce_log(LOG_LEVEL::ERROR, message);
     std::exit(exit_code);
 }
--- src/position/util.hpp
+++ src/position/util.hpp
@@ -4,7 +4,7 @@
 
 /**
  * @brief Raise a critical error and exit
- * 
+ *
  * @param message The message to display to stdout
  * @param exit_code Exit code, usually 1 or 2
  */
